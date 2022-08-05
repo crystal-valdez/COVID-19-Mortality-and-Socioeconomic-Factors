e8b73f1f423c7dd0c384915bad36dd51551deab1
Papiha Joharapurkar <papihajoharapurkar@user3-226-49.wireless.utoronto.ca>
2022-08-05 11:04:36 -0400

    Stratified by country

--- Task1.R
+++ Task1.R
@@ -1,7 +1,21 @@
+#Global Counts 
 library(httr)
 
 res <- VERB("GET", url = "https://covid19-stats-api.herokuapp.com/api/v1/cases?")
 
 cat(content(res, 'text'))
+
+totalconfirmed_cases <- content(res)$confirmed
+totalconfirmed_deaths <- content(res)$deaths
+totalconfirmed_recovered <- content(res)$recovered
+
+
+#Counts by Country
+library(httr)
+
+res <- VERB("GET", url = "https://covid19-stats-api.herokuapp.com/api/v1/cases/country/confirmed")
+content(res)
+cat(content(res, 'text'))
+
 
 
