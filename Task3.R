602b01f6501f4c5b41f4395a5cab8bac1a262e12
Papiha Joharapurkar <papihajoharapurkar@user3-226-49.wireless.utoronto.ca>
2022-08-05 14:19:02 -0400

    Regex

--- Task3.R
+++ Task3.R
@@ -2,13 +2,23 @@
 # task 3 
 library("wbstats")
 library(httr)
+library(stringr)
 
-pops2020 <- wb_data("SP.POP.TOTL", country = "all", start_date = 2020, end_date = 2020)
+# pops2020 <- wb_data("SP.POP.TOTL", country = "all", start_date = 2020, end_date = 2020)
 
-pops2021 <- wb_data("SP.POP.TOTL", country = "all", start_date = 2021, end_date = 2021)
+pops2021 <- as.data.frame(wb_data("SP.POP.TOTL", country = "all", start_date = 2021, end_date = 2021))
+
+##regex attempt -- help plz 
+pops2021 %>% str_extract(iso2c, [[:digit:]]) 
+
+
+
+
+
 
 # sum for 2021 
 sum(as.numeric(pops2021$SP.POP.TOTL), na.rm = T)
 
 # sum for 2020
 sum(as.numeric(pops2020$SP.POP.TOTL), na.rm = T)
+
