e304c789a7c36fda4549bbd8c0ac92f9deeb5a8b
Papiha Joharapurkar <papihajoharapurkar@user3-226-49.wireless.utoronto.ca>
2022-08-05 11:48:44 -0400

    Added sub lists for country and counts

--- Task1.R
+++ Task1.R
@@ -10,12 +10,28 @@ totalconfirmed_deaths <- content(res)$deaths
 totalconfirmed_recovered <- content(res)$recovered
 
 
-#Counts by Country
+#Confirmed Counts by Country
 library(httr)
-
 res <- VERB("GET", url = "https://covid19-stats-api.herokuapp.com/api/v1/cases/country/confirmed")
-content(res)
-cat(content(res, 'text'))
+country_list <- list()
+count_list <- list()
+
+#removing 1 row of list 
+unlisted_res <- unlist(content(res),recursive=FALSE)
+
+#adding countries to separate list 
+for (i in 1:length(unlisted_res)) {
+  if (i %% 2 == 0) {
+    country_list <- c(country_list,unlisted_res[i])
+  }
+}
+
+#adding counts to separate list 
+for (i in 1:length(unlisted_res)) {
+  if (i %% 2 != 0) {
+    count_list <- c(count_list,unlisted_res[i])
+  }
+}
 
 
 
