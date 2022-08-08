d002c21443a96f53701db31aaec47efd51715162
Papiha Joharapurkar <papihajoharapurkar@user3-226-49.wireless.utoronto.ca>
2022-08-08 12:57:44 -0400

    Added world-population metrics

--- Task3.R
+++ Task3.R
@@ -32,3 +32,19 @@ pops2021_2 <- filter(pops2021_2, !grepl("Union", country))
 world_pop <- sum(as.numeric(pops2021$SP.POP.TOTL), na.rm = T)
 
 
+#copying code from Task 1, afterwards import in RDS object to obtain confirmed, deaths and recovered 
+library(httr)
+
+res <- VERB("GET", url = "https://covid19-stats-api.herokuapp.com/api/v1/cases?")
+
+cat(content(res, 'text'))
+
+totalconfirmed_cases <- content(res)$confirmed
+totalconfirmed_deaths <- content(res)$deaths
+totalconfirmed_recovered <- content(res)$recovered
+
+world_pop/totalconfirmed_cases
+
+world_pop/totalconfirmed_deaths
+
+world_pop/totalconfirmed_recovered
