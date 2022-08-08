2c928e71c08b2d35c62b9b6ceba6e35a45d94de0
Deweena <deweenaparija@utmuser58-108.wireless.utoronto.ca>
2022-08-08 12:59:26 -0400

    Cumulation of Task 3 + 4

--- Task4.R
+++ Task4.R
@@ -1,2 +1,55 @@
-## Creating country categories
+#Task 3 – Combine data to calculate the ratio of all types of COVID-19 cases 
+#(confirmed, recovered, death/mortality-rate) to the population 
+#(mortality rate = # of deaths in population per unit of time) 
+
+# task 3 
+library("wbstats")
+library(httr)
+library(stringr)
+library(tidyr)
+
+pops2021 <- as.data.frame(wb_data("SP.POP.TOTL", country = "all", start_date = 2021, end_date = 2021))
+
+###removing iso2c columns including numbers and NA-values 
+pops2021_2 <- filter(pops2021, !grepl("[0-9]", iso2c)) 
+pops2021_2 <- pops2021_2[!is.na(pops2021_2$iso2c),]
+pops2021_2 <- pops2021_2 %>% drop_na(iso2c)
+
+pops2021_2 <- filter(pops2021_2, !grepl("countries", country)) 
+pops2021_2 <- filter(pops2021_2, !grepl("only", country)) 
+pops2021_2 <- filter(pops2021_2, !grepl("income", country)) 
+pops2021_2 <- filter(pops2021_2, !grepl("total", country)) 
+pops2021_2 <- filter(pops2021_2, !grepl("blend", country)) 
+pops2021_2 <- filter(pops2021_2, !grepl("Union", country))
+pops2021_2 <- filter(pops2021_2, !grepl("&", country))
+pops2021_2 <- filter(pops2021_2, !grepl("area", country))  
+pops2021_2 <- filter(pops2021_2, !grepl("members", country))  
+pops2021_2 <- filter(pops2021_2, !grepl("North America", country))  
+pops2021_2 <- filter(pops2021_2, !grepl("Sub", country))  
+pops2021_2 <- filter(pops2021_2, !grepl("Union", country))  
+
+# sum of population for 2021 
+world_pop <- sum(as.numeric(pops2021$SP.POP.TOTL), na.rm = T)
+
+
+#copying code from Task 1, afterwards import in RDS object to obtain confirmed, deaths and recovered 
+library(httr)
+
+res <- VERB("GET", url = "https://covid19-stats-api.herokuapp.com/api/v1/cases?")
+
+cat(content(res, 'text'))
+
+totalconfirmed_cases <- content(res)$confirmed
+totalconfirmed_deaths <- content(res)$deaths
+totalconfirmed_recovered <- content(res)$recovered
+
+world_pop/totalconfirmed_cases
+
+world_pop/totalconfirmed_deaths
+
+world_pop/totalconfirmed_recovered
+
+# Task 4
+
+
 
