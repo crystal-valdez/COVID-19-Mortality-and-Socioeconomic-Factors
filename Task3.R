fd93681bd31edc04e99a554705fa6b8dae0223a8
Papiha Joharapurkar <papihajoharapurkar@user3-226-49.wireless.utoronto.ca>
2022-08-08 11:39:40 -0400

    Cleaned code, removed NA values and number-values from ISO2C column

--- Task3.R
+++ Task3.R
@@ -5,23 +5,14 @@ library(httr)
 library(stringr)
 library(tidyr)
 
-# pops2020 <- wb_data("SP.POP.TOTL", country = "all", start_date = 2020, end_date = 2020)
-
 pops2021 <- as.data.frame(wb_data("SP.POP.TOTL", country = "all", start_date = 2021, end_date = 2021))
 
-##regex attempt -- help plz 
-pops2021 %>% str_extract(iso2c, [[:digit:]]) 
+###removing iso2c columns including numbers and NA-values 
+pops2021_2 <- filter(pops2021, !grepl("[0-9]", iso2c)) 
+pops2021_2 <- pops2021_2[!is.na(pops2021_2$iso2c),]
+View(pops2021_2)
 
-pops2021 %>% filter(pops2021, grepl("[0-9]") == T)
-
-### without numbers 
-
-pops2021.2 <- filter(pops2021, !grepl("[0-9]", iso2c))
-
-
-drop_na(pops2021.2$iso2c)
-
-# sum for 2021 
+# sum of population for 2021 
 sum(as.numeric(pops2021$SP.POP.TOTL), na.rm = T)
 
 # sum for 2020
