7ac5b8904c5a0d970611b7357fe4bf15f647f1ae
Alana McPhail <alanamcphail@utmuser174-68.wireless.utoronto.ca>
2022-08-08 11:31:47 -0400

    with removal of numbers in iso2c

--- Task3.R
+++ Task3.R
@@ -3,6 +3,7 @@
 library("wbstats")
 library(httr)
 library(stringr)
+library(tidyr)
 
 # pops2020 <- wb_data("SP.POP.TOTL", country = "all", start_date = 2020, end_date = 2020)
 
@@ -11,10 +12,14 @@ pops2021 <- as.data.frame(wb_data("SP.POP.TOTL", country = "all", start_date = 2
 ##regex attempt -- help plz 
 pops2021 %>% str_extract(iso2c, [[:digit:]]) 
 
+pops2021 %>% filter(pops2021, grepl("[0-9]") == T)
+
+### without numbers 
+
+pops2021.2 <- filter(pops2021, !grepl("[0-9]", iso2c))
 
 
-
-
+drop_na(pops2021.2$iso2c)
 
 # sum for 2021 
 sum(as.numeric(pops2021$SP.POP.TOTL), na.rm = T)
